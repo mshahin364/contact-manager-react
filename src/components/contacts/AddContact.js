72469a4796707e802e7bc8958900416f40a183d5
Shahinur Islam <mshahin364@gmail.com>
2018-07-28 23:51:17 +0600

    Uncontrolled Components using refs

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -1,20 +1,35 @@
 import React, { Component } from "react";
 
 class AddContact extends Component {
-  state = {
-    name: "",
-    email: "",
-    phone: ""
-  };
+  constructor(props) {
+    super(props);
+
+    this.nameInput = React.createRef();
+    this.emailInput = React.createRef();
+    this.phoneInput = React.createRef();
+
+  }
 
   onSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
-  }
-  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    const contact = {
+      name: this.nameInput.current.value,
+      email: this.emailInput.current.value,
+      phone: this.phoneInput.current.value,
+    }
+
+    console.log(contact);
+  };
+
+  static defaultProps = {
+    name: "Fred Smith",
+    email: "fred@yahoo.com",
+    phone: "777-777-7777"
+  };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone } = this.props;
 
     return (
       <div className="card mb-3">
@@ -28,8 +43,8 @@ class AddContact extends Component {
                 name="name"
                 className="form-control form-control-lg"
                 placeholder="Enter Name..."
-                value={name}
-                onChange={this.onChange}
+                defaultValue={name}
+                ref={this.nameInput}
               />
             </div>
 
@@ -40,8 +55,8 @@ class AddContact extends Component {
                 name="email"
                 className="form-control form-control-lg"
                 placeholder="Enter Email..."
-                value={email}
-                onChange={this.onChange}
+                defaultValue={email}
+                ref={this.emailInput}
               />
             </div>
 
@@ -52,8 +67,8 @@ class AddContact extends Component {
                 name="phone"
                 className="form-control form-control-lg"
                 placeholder="Enter Phone..."
-                value={phone}
-                onChange={this.onChange}
+                defaultValue={phone}
+                ref={this.phoneInput}
               />
             </div>
             <input type="submit" value="Add Contact" className="btn btn-light btn-block" />
